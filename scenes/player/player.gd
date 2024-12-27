870b1ec576a4a919f59af04a9c7bd7325597196c
Niamh <niamh.linguist@gmail.com>
2024-12-27 22:30:44 +0200

    basic player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,13 +3,36 @@ extends CharacterBody2D
 class_name Player
 
 const GRAVITY: float = 690.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const JUMP_VELOCITY: float = -260.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
+	
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 	
+	get_input()
 	move_and_slide()
+	
+func get_input() -> void:
+	
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
