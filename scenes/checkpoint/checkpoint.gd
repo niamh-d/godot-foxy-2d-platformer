f3743125bcf77f9b0b22373c52bd77e961466bcc
Niamh <niamh.linguist@gmail.com>
2025-01-01 23:24:45 +0200

    parallax

--- scenes/checkpoint/checkpoint.gd
+++ scenes/checkpoint/checkpoint.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+const TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
 
-# Called when the node enters the scene tree for the first time.
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var sound: AudioStreamPlayer2D = $Sound
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalManager.on_boss_killed.connect(on_boss_killed)
 
+func on_boss_killed(_p: int) -> void:
+	sprite_2d.show()
+	animation_tree[TRIGGER_CONDITION] = true
+	monitoring = true
+	SoundManager.play_clip(sound, SoundManager.SOUND_CHECKPOINT)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_entered(area: Area2D) -> void:
+	SoundManager.play_clip(sound, SoundManager.SOUND_WIN)
